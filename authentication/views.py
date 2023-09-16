62154801c05b2c6ce7398b168cf6785b3e4e04d4
Kaushal Patel <patelkaushalb1@gmail.com>
2023-09-15 22:05:26 -0400

    Added signup post call for backend

--- authentication/views.py
+++ authentication/views.py
@@ -6,6 +6,15 @@ def home(request):
     return render(request, 'authentication/index.html')
 
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        email = request.POST['email']
+        password = request.POST['pass1']
+        confirm_password = request.POST['pass2']
+
+
     return render(request, 'authentication/signup.html')
 
 def login_request(request):
