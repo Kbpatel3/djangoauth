86880985804cd167d5a6efb2fdbe0477f0bfc2c7
Kaushal Patel <patelkaushalb1@gmail.com>
2023-09-15 21:51:57 -0400

    Rendered main index page with signup, login, logout buttons

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello, World!')
+    return render(request, 'authentication/index.html')
+
+def signup(request):
+    return render(request, 'authentication/signup.html')
+
+def login_request(request):
+    return render(request, 'authentication/login.html')
+
+def logout_request(request):
+    pass
